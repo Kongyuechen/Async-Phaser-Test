74a959eeb000480a7d3829e66f36550e878d6b38
Kong Yue Chen <kongyue.chen@gmail.com>
2021-11-05 00:05:34 -0400

    Added Movement and  World Collision

--- client/scenes/TestPhysics.js
+++ client/scenes/TestPhysics.js
@@ -8,16 +8,36 @@ export default class TestPhysics extends Phaser.Scene {
   }
 
   create() {
+    // Adding Text To Middle Of Screen
     const text = this.add.text(400,250, 'Testing Physics ');
     text.setOrigin(0.5, 0.5);
     // Adding Circle Object
-    const circleObj = this.add.circle(400, 150, 10, 0xffffff, 1)
-    this.physics.add.existing(circleObj)
+    this.circleObj = this.add.circle(400, 150, 10, 0xffffff, 1)
+    this.physics.add.existing(this.circleObj)
+    this.circleObj.body.setCollideWorldBounds(true)
     // Adding A Rectangle Object As Platform
     const rectangleObj = this.add.rectangle(400, 300, 200, 20, 0xffffff, 1)
     this.physics.add.existing(rectangleObj, true)
 
-    // Adding Collision function
-    this.physics.add.collider(rectangleObj, circleObj)
+    // Adding Collision Function
+    this.physics.add.collider(rectangleObj, this.circleObj)
+
+    // Keyboard Plugin
+    this.cursors = this.input.keyboard.createCursorKeys()
+  }
+
+  update() {
+    if(this.cursors.space.isDown){
+      // this.circleObj.StaticBody.setVelocityY(-100)
+      this.circleObj.y -= 10
+    }else if (this.cursors.left.isDown){
+      this.circleObj.x -= 10
+
+      // this.circleObj.StaticBody.setVelocityX(-100)
+    }else if (this.cursors.right.isDown){
+      this.circleObj.x += 10
+
+      // this.circleObj.StaticBody.setVelocityX(100)
+    }
   }
 }
